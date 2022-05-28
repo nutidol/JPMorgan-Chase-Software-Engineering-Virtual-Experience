fc5c71c15a37c37eb9b9a03057451fdca4c68474
Nat Sae Tang <56648932+nutidol@users.noreply.github.com>
2022-05-28 16:50:56 +0100

    Add files via upload

---
+++ JPMC-tech-task-3-PY3/build/precache-manifest.4e93c7d284f190eb0df21c55dea7a50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "122808c9b066bca69b03",
+    "url": "/static/css/main.ae58442e.chunk.css"
+  },
+  {
+    "revision": "122808c9b066bca69b03",
+    "url": "/static/js/main.122808c9.chunk.js"
+  },
+  {
+    "revision": "3f1bdfaa5cd262bbbb06",
+    "url": "/static/css/1.5b03363e.chunk.css"
+  },
+  {
+    "revision": "3f1bdfaa5cd262bbbb06",
+    "url": "/static/js/1.3f1bdfaa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cb125cdd0b87d96bcb81fcaf882aa23",
+    "url": "/index.html"
+  }
+];
